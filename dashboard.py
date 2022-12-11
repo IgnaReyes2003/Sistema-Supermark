35500e2ca7d88ecd4b96804d5ea9949cef562aa0
IgnacioRisso <gamerdream406@gmail.com>
2022-12-11 15:50:49 -0300

    update
    Adición de iconos.

--- dashboard.py
+++ dashboard.py
@@ -1,12 +1,15 @@
 from tkinter import*
+from PIL import Image,ImageTk #Es necesario instalar pip pillow.
 class IMS:
+    
     def __init__(self, root):
         self.root=root
         self.root.geometry("1350x700+0+0")
         self.root.title("Sistema de Gestión Supermark | Desarrollado por Ignacio Reyes CM1")
 
-        # ====Título====  
-        title=Label(self.root, text="Sistema de Gestión Supermark", font=("Consolas",38,"bold"),bg="#ffde0a",fg="black").place(x=0,y=0,relwidth=1,height=70) 
+        # ====Título====
+        self.icon_title=PhotoImage(file="images/cart.png")  
+        title=Label(self.root, text="Sistema de Gestión Supermark",image=self.icon_title,compound=LEFT, font=("Arial Rounded MT Bold",40,"bold"),bg="#014070",fg="black",anchor="w",padx=20).place(x=0,y=0,relwidth=1,height=70) 
 
 root=Tk()
 obj = IMS(root)
