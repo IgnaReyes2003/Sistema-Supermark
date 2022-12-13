d58963113afb31e1993946ebbd63e38f32869ef2
IgnacioRisso <gamerdream406@gmail.com>
2022-12-13 17:01:42 -0300

    Update employee

--- employee.py
+++ employee.py
@@ -31,7 +31,7 @@ class employeeClass:
         SearchFrame.place(x=250,y=20,width=600,height=70)
 
         #===== Opciones =====
-        cmb_search=ttk.Combobox(SearchFrame,textvariable=self.var_searchby,values=("Seleccione","Email","Nombre","Contacto"),state="readonly",justify=CENTER,font=("time new roman",15))
+        cmb_search=ttk.Combobox(SearchFrame,textvariable=self.var_searchby,values=("Seleccionar","Email","Nombre","Contacto"),state="readonly",justify=CENTER,font=("time new roman",15))
         cmb_search.place(x=10,y=10,width=180)
         cmb_search.current(0)
 
@@ -42,13 +42,29 @@ class employeeClass:
         title = Label(self.root,text="Datos del empleado",font=("times new roman",15,"bold"),bg="#0f4d7d",fg="black").place(x=50,y=100,width=1000)
 
         #===== Contenido =====
+        #===== Fila 1 ========
         lbl_empid = Label(self.root,text="Emp ID",font=("times new roman",15),bg="white").place(x=50,y=150)
         lbl_gender = Label(self.root,text="Género",font=("times new roman",15),bg="white").place(x=350,y=150)
         lbl_contact = Label(self.root,text="Contacto",font=("times new roman",15),bg="white").place(x=750,y=150)
 
-        txt_empid = Entry(self.root,text="Emp ID",font=("times new roman",15),bg="white").place(x=50,y=150)
-        txt_gender = Entry(self.root,text="Género",font=("times new roman",15),bg="white").place(x=350,y=150)
-        txt_contact = Entry(self.root,text="Contacto",font=("times new roman",15),bg="white").place(x=750,y=150)
+        txt_empid = Entry(self.root,textvariable=self.var_emp_id,font=("times new roman",15),bg="lightyellow").place(x=150,y=150,width=180)
+        cmb_gender=ttk.Combobox(self.root,textvariable=self.var_gender,values=("Seleccionar","Hombre","Mujer","Otro"),state="readonly",justify=CENTER,font=("time new roman",15))
+        cmb_gender.place(x=500,y=150,width=180)
+        cmb_gender.current(0)
+        txt_contact = Entry(self.root,textvariable=self.var_contact,font=("times new roman",15),bg="lightyellow").place(x=850,y=150,width=180)
+
+        #===== Fila 2 ========
+
+        lbl_name = Label(self.root,text="Nombre",font=("times new roman",15),bg="white").place(x=50,y=190)
+        #D.O.B (date of birth) se refiere a la fecha de nacimiento. 
+        lbl_dob = Label(self.root,text="F.D.N",font=("times new roman",15),bg="white").place(x=350,y=190)
+        #D.O.J (date of joining) se refiere a la fecha de inscripción.
+        lbl_doj = Label(self.root,text="F.D.I",font=("times new roman",15),bg="white").place(x=750,y=190)
+
+        txt_name = Entry(self.root,textvariable=self.var_name,font=("times new roman",15),bg="lightyellow").place(x=150,y=190,width=180)
+        txt_dob = Entry(self.root,textvariable=self.var_dob,font=("times new roman",15),bg="lightyellow").place(x=500,y=190,width=180)
+        txt_doj = Entry(self.root,textvariable=self.var_doj,font=("times new roman",15),bg="lightyellow").place(x=850,y=190,width=180)
+
 
 if __name__=="__main__":
     root=Tk()
