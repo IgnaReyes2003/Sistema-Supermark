062058edd6efa7a25d49a38f012317c9adf287d5
IgnacioRisso <gamerdream406@gmail.com>
2023-01-27 21:35:21 -0300

    Updates

--- login.py
+++ login.py
@@ -26,7 +26,7 @@ class Login_System:
         login_frame=Frame(self.root,bd=2,relief=RIDGE,bg="white")
         login_frame.place(x=650,y=90,width=350,height=460)
 
-        title=Label(login_frame,text="Inicio de Sesión",font=("Elephant",30,"bold"),bg="white").place(x=0,y=30,relwidth=1)
+        title=Label(login_frame,text="Inicio de Sesión",font=("Bell MT",34,"bold"),bg="white").place(x=0,y=30,relwidth=1)
         lbl_user=Label(login_frame,text="Empleado ID",font=("Andalus",15),bg="white",fg="#767171").place(x=46,y=100)
 
         txt_employee_id=Entry(login_frame,textvariable=self.employee_id,font=("times new roman",15),bg="#ECECEC").place(x=50,y=140,width=250)
@@ -59,7 +59,7 @@ class Login_System:
         self.lbl_change_image.place(x=337,y=145,width=241,height=430)
 
         self.animate()
-        self.send_email("xyz")
+        #self.send_email("xyz")
 
 #=================== Todas las funciones ==================
 
@@ -123,7 +123,7 @@ class Login_System:
                         title=Label(self.forget_win,text="Restablecer contraseña",font=("times new roman",15,"bold"),bg="#3f51b5",fg="white").pack(side=TOP,fill=X)
                         lbl_reset=Label(self.forget_win,text="Ingresar la clave enviada a su email",font=("times new roman",15)).place(x=20,y=60)
                         txt_reset=Entry(self.forget_win,textvariable=self.var_otp,font=("times new roman",15),bg="lightyellow").place(x=20,y=100,width=250,height=30)
-                        self.btn_reset=Button(self.forget_win,text="Enviar",font=("times new roman",15),bg="lightblue")
+                        self.btn_reset=Button(self.forget_win,text="Enviar",command=self.validate_otp,font=("times new roman",15),bg="lightblue")
                         self.btn_reset.place(x=280,y=100,width=100,height=30)
 
                         lbl_new_pass=Label(self.forget_win,text="Nueva contraseña",font=("times new roman",15)).place(x=20,y=160)
@@ -138,6 +138,16 @@ class Login_System:
         except Exception as ex:
             messagebox.showerror("Error",f"Error causador por: {str(ex)}",parent=self.root)
 
+    def update_password(self):
+        if self.var_new_pass.get()=="" or self.var_conf_pass:
+
+    def validate_otp(self):
+        if int(self.otp)==int(self.var_otp.get()):
+            self.btn_update.config(state=NORMAL)
+            self.btn_reset.config(state=DISABLED)
+        else:
+            messagebox.showerror("Error","Clave incorrecta, inténtalo de nuevo",parent=self.forget_win)
+
     def send_email(self,to_):
         s=smtplib.SMTP("smtp.gmail.com",587)
         s.starttls()
@@ -146,13 +156,13 @@ class Login_System:
 
         s.login(email_,pass_)
 
-        #self.otp=str(time.strftime("%H%S%M"))+str(time.strftime("%S"))
         self.otp=int(time.strftime("%H%S%M"))+int(time.strftime("%S"))
         
         subj="SGS-Reestablecer contraseña"
         msg=f"Dear Sir/Madamm,\n\ntu contraseña {str(self.otp)}.\n\n with regards,\nSGS Team"
         msg="Subject:{}\n\n{}".format(subj,msg)
-        s.sendmail(email_,to_,msg)
+        
+        s.sendmail(email_,to_,msg.encode("utf8"))
         chk=s.ehlo()
 
         if chk[0]==250:
